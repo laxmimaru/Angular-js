7c306b6b20bbb7c56db83bd284d64021a8d19eba
Laxmi Kanth <laxmimaru28@gmail.com>
2023-12-21 01:39:04 +0530

    toDo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToDo } from '../app/toDo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular demo';
-  date = new Date();
-  name = "Laxmi Kanth";
+  toDoValue: string = '';
+  id: number = 0;
+  list: ToDo[] = [];
+
+  ngOnInit() {
+    this.id = 0;
+    this.list = [];
+    this.toDoValue = '';
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked = ', this.list, 'id = ', this.id);
+  }
+
+  addTask(newToDovalue: string) {
+    const newItem: ToDo = {
+      id: this.id + 1,
+      toDoValue: newToDovalue,
+      isDone: false,
+    };
+    this.list.push(newItem);
+    this.toDoValue = '';
+  }
+
+  deleteTask(deleteToDo: string) {
+    console.log('deleteToDo = ', deleteToDo);
+    this.list = this.list.filter((task) => task.toDoValue !== deleteToDo);
+  }
 }
---
+++ src/app/toDo.ts
@@ -0,0 +1,5 @@
+export interface ToDo{
+    id:number;
+    toDoValue:string;    
+    isDone:boolean;
+}
